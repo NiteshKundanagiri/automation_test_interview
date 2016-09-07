5493330ec305e48274bca6f950a378081c444a30
NiteshKundanagiri <nkundanagiri@gmail.com>
2016-09-07 17:57:18 -0400

    Updated GoogleExampleIT.java
    Please check the file. I have updated it

--- src/test/java/GoogleExampleIT.java
+++ src/test/java/GoogleExampleIT.java
@@ -8,40 +8,76 @@
  * 
  * */
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-@Test(groups={"localTest"})
 public class GoogleExampleIT {
 	private static final Logger LOGGER = LogManager.getLogger(GoogleExampleIT.class.getName());
 	private WebDriver driver;
 	@FindBy(css = "[name='q']")
 	private WebElement searchBar;
 
-	@Test
-	public void googleCheeseExample() throws Exception {
+	/*
+	 * initializing browser and navigating to link "http://www.google.com"
+	 */
+	@BeforeMethod
+	public void setUp() {	
+		driver= new FirefoxDriver();
+		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 60), this);
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+	}
+
+	/*
+	 * searching for word "Cheese!" and validating title of page
+	 */
+	@Test(groups={"localTest"})
+	public void googleCheeseExample()throws Exception {
 		searchBar.clear();
 		searchBar.sendKeys("Cheese!");
 		searchBar.sendKeys(Keys.ENTER);
+		Thread.sleep(1000);
 		LOGGER.info("Page title is: " + driver.getTitle());
-		Assert.assertTrue(driver.getTitle().equals("Google"));
+		/*
+		Assert.assertTrue(driver.getTitle().equals("Google")); 
+		Page title for search is "Cheese! - Google Search" or 
+		should use assertFalse when comparing to "Google"
+		 */
+		Assert.assertTrue(driver.getTitle().equals("Cheese! - Google Search"));
 	}
 
-	@Test(enabled = true)
+	/*
+	 * searching for word "Milk!" and validating title of page
+	 */
+	@Test(groups={"localTest"})
 	public void googleMilkExample() throws Exception {
 		searchBar.clear();
 		searchBar.sendKeys("Milk!");
 		searchBar.sendKeys(Keys.ENTER);
-		LOGGER.info("Page title is: {}", driver.getTitle());
-		Assert.assertTrue(driver.getTitle().equals("Google"));
+		Thread.sleep(1000);
+		LOGGER.info("Page title is: " + driver.getTitle());
+		/*
+		Assert.assertTrue(driver.getTitle().equals("Google")); 
+		Page title for search is "Milk! - Google Search" or 
+		should use assertFalse when comparing to "Google"
+		 */
+		Assert.assertTrue(driver.getTitle().equals("Milk! - Google Search"));
 	}
 
-	@BeforeMethod
-	public void setUp() {
-		
-		driver = new new FirefoxDriver();
-		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 60), this);
-		driver.get("http://www.google.com");
-	}
-
+	/*
+	 * closing browser
+	 */
 	@AfterMethod
 	public void tearDown() {
 		driver.close();
